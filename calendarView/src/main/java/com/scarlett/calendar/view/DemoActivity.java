f107f1a4e601b877480d9d09fde52820bb3e4ac0
scarlettli <scarlettli87@gmail.com>
2014-12-12 23:51:13 +0800

    add calendar view demo

---
+++ calendarView/src/main/java/com/scarlett/calendar/view/DemoActivity.java
@@ -0,0 +1,22 @@
+package com.scarlett.calendar.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class DemoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_demo);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.demo, menu);
+		return true;
+	}
+
+}
